99e248482a02e7e5c26548582f91b0768f462663
@Maksim-Gibkin <gostkabo@gmail.com>
2022-08-14 15:39:10 +0300

    Добавлены роуты

---
+++ demo/src/components/AppRouter.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+import { Route, Routes } from 'react-router-dom'
+import { privateRoutes, publicRoutes } from "../router";
+
+const AppRouter:FC = () => {
+    const auth = false;
+    return (
+        auth
+        ? (<Routes>
+                {privateRoutes.map((route) =>
+                    <Route path={route.path} element={route.component} key={route.path}/>
+                )}
+            </Routes>)
+        : (<Routes>
+                {publicRoutes.map((route) =>
+                    <Route path={route.path} element={route.component} key={route.path}/>
+                )}
+        </Routes>)
+    );
+};
+
+export default AppRouter;
--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom'
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <Provider store={}>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
